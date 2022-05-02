f24d45e4dc901aa0cbd1124d4c2f4584e625a534
Mete <metecontay@gmail.com>
2022-05-02 11:22:34 +0200

    daire tribi

--- src/Giris/Triangle.java
+++ src/Giris/Triangle.java
@@ -1,5 +1,17 @@
 package Giris;
 
+import java.util.Scanner;
+
 public class Triangle {
-    //Üçgenin iç açkılarını hesaplayacağız
+    public static void main(String[] args) {
+        int r;
+        double pi = 3.14;
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Dairenin yarıçapını giriniz : ");
+        r = inp.nextInt();
+        double alan = pi * r * r;
+        double cevre = 2 * pi * r;
+        System.out.println("Dairenin alanı : " + alan);
+        System.out.println("Dairenin çevresi : " + cevre);
+    }
 }
