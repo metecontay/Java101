82b07191748f8675db754dd3a0e2c2c7a879fa14
Mete <metecontay@gmail.com>
2022-05-03 11:09:18 +0200

    hesapmakinesi added

---
+++ src/Lessons2/hesapmakinesi.java
@@ -0,0 +1,37 @@
+package Lessons2;
+
+import java.util.Scanner;
+
+public class hesapmakinesi {
+    public static void main(String[] args) {
+        int n1,n2,select;
+        Scanner input = new Scanner(System.in);
+        System.out.print("İlk sayıyı giriniz : ");
+        n1 = input.nextInt();
+        System.out.print("İkinci sayıyı giriniz : ");
+        n2 = input.nextInt();
+
+        System.out.println("1-Toplama\n2-Çıkarma\n3-Çarpma\n4-Bölme");
+        System.out.print("Seçiminiz : ");
+        select = input.nextInt();
+
+        switch (select){
+            case 1:
+            System.out.println("Toplama : " + (n1 + n2));
+            break;
+            case 2:
+            System.out.println("Çıkarma : " + (n1 - n2));
+            break;
+            case 3:
+                System.out.println("Çarpma : " + (n1 * n2));
+                break;
+            case 4:
+                System.out.println("Bölme : " + (n1 / n2));
+                System.out.println("Hiçbir sayı 0 ile bölünemez. Tekrar deneyiniz.");
+                break;
+            default:
+                    System.out.println("Yanlış ifade girdiniz. Lütfen tekrar deneyiniz");
+             break;
+        }
+    }
+}
