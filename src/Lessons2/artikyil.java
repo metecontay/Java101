be24a0cd9672d41eca92656362baa2f7dfcce772
Mete <metecontay@gmail.com>
2022-05-10 20:33:31 +0200

    artikyil

---
+++ src/Lessons2/artikyil.java
@@ -0,0 +1,18 @@
+package Lessons2;
+import java.util.Scanner;
+public class artikyil {
+
+
+        public static void main(String[] args) {
+            int yil;
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Yil Giriniz: ");
+            yil=scan.nextInt();
+
+            if (yil%4==0 || yil%400==0)
+                System.out.print(yil + " Bir Artik Yildir ");
+            else
+                System.out.println(yil + " Bir Artik Yil Degildir");
+        }
+    }
+
